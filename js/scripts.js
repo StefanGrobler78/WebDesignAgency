038d70100a5971e32ab9ac010a8adc75b3ae4c68
Stefan Grobler <stefangrobler78@gmail.com>
2024-03-21 04:47:23 +0200

    remove logs

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,7 @@ let menuBox = document.querySelector('.nav__list')
 let menuIcon = document.querySelector('.toggle__icon')
 
 menuIcon.addEventListener('click', () => {
-    menuBox.classList.toggle('open-menu')
-    console.log('hello');
-    
+    menuBox.classList.toggle('open-menu')    
 })
 
 let links = document.querySelectorAll('.nav__link')
